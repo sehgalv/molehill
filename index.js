975c730207296828f8ccaf109da644cf3f6b07fe
sehgalv <sehgalv@vcu.edu>
2020-06-20 19:56:48 -0400

    (feat): add initial test insert statement - adds comment with reference guide

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const config = {
     schema: 'molehill_test'
 };
 
+// Reference for mysqlx lib: https://dev.mysql.com/doc/x-devapi-userguide/en/devapi-users-working-with-relational-tables.html
 mysqlx.getSession(config)
     .then(session => {
         console.log(session.inspect()); 
+        const molehillDB = session.getSchema('molehill_test');
+        
+        const productTable = molehillDB.getTable('walmart_test');
+
+        // Insert SQL Table data
+        productTable.insert(['walmart_id','weight','d1','d2','d3','category','title','url']).
+            values(1, 100, 10, 12, 14, 'Test Category', 'My test prod', 'walmart.com/test-prod').execute();
     });
